194cc243183e4ac65ee090914282205d0ac2e97e
SARVESHWAR SINGH <sarveshwar@emurgo.io>
2019-07-22 19:10:07 +0900

    test

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let tWindow;
+
+function onInputChange() {
+  tWindow.postMessage('Test from A', 'http://localhost:3001/');
+  console.log('changing');
+}
+
+function onClickButton() {
+  tWindow  = window.open('http://localhost:3001/');
+  console.log('onClickButton');
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={onClickButton}>Open New Window</button>
+        <br/>
+        <input onChange={onInputChange}></input>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,7 +31,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 2
         </a>
       </header>
     </div>
